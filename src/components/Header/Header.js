9687fa59747c564f73bf6b8a4a325aae7dcc0899
adavkay <andykay.me@gmail.com>
2015-07-30 21:46:50 -0400

    header with logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,78 +10,78 @@ require('./Header.scss');
 
 class Header extends React.Component {
 
-	constructor() {
+  constructor() {
     super();
 
-		this.state = LoginStore.getState();
+    this.state = LoginStore.getState();
 
-		this.handleChange = this.handleChange.bind(this);
-		this.handleLogout = this.handleLogout.bind(this);
-	}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
 
-	componentDidMount() {
-		LoginStore.listen(this.handleChange);
-	}
+  componentDidMount() {
+    LoginStore.listen(this.handleChange);
+  }
 
-	componentWillUnmount() {
-		LoginStore.unlisten(this.handleChange);
-	}
+  componentWillUnmount() {
+    LoginStore.unlisten(this.handleChange);
+  }
 
-	handleChange(state) {
-		this.setState(state);
-	}
+  handleChange(state) {
+    this.setState(state);
+  }
 
-	handleLogout(e) {
-		e.preventDefault();
-		LoginActions.logoutUser();
-	}
+  handleLogout(e) {
+    e.preventDefault();
+    LoginActions.logoutUser();
+  }
 
-	render() {
-		let loginNavItem;
-		let signupNavItem;
-		let profileDropdown;
+  render() {
+    let loginNavItem;
+    let signupNavItem;
+    let profileDropdown;
 
-		if (LoginStore.isLoggedIn()) {
-			// if user is logged in show logout link
-			loginNavItem = <NavItem onClick={this.handleLogout}>logout</NavItem>;
-			signupNavItem = null;
+    if (LoginStore.isLoggedIn()) {
+      // if user is logged in show logout link
+      loginNavItem = <NavItem onClick={this.handleLogout}>logout</NavItem>;
+      signupNavItem = null;
 
-			profileDropdown = <DropdownButton eventKey={3} title={
-													<ProfilePhoto
-														width={35}
-														height={35}
-														photo={this.state.user.photo }
-													/>
-												}
-												className="profileDropdown"
-												noCaret={true}
-												>
-													<MenuItem eventKey='1'><Link to="user-profile" params={{ username: this.state.user.username }}>View Profile</Link></MenuItem>
-													<MenuItem eventKey='2'>Another action</MenuItem>
-													<MenuItem eventKey='3'>Something else here</MenuItem>
-													<MenuItem divider />
-													<MenuItem eventKey='4'>Separated link</MenuItem>
-												</DropdownButton>;
-		} else {
-			// if user is not logged in show login link
-			loginNavItem = <NavItemLink to={`/login`}>login</NavItemLink>;
-			signupNavItem = <NavItemLink to={`/signup`}>signup</NavItemLink>;
+      profileDropdown = <DropdownButton eventKey={3} title={
+                          <ProfilePhoto
+                            width={35}
+                            height={35}
+                            photo={this.state.user.photo }
+                          />
+                        }
+                        className="profileDropdown"
+                        noCaret={true}
+                        >
+                          <MenuItem eventKey='1'><Link to="user-profile" params={{ username: this.state.user.username }}>View Profile</Link></MenuItem>
+                          <MenuItem eventKey='2'>Another action</MenuItem>
+                          <MenuItem eventKey='3'>Something else here</MenuItem>
+                          <MenuItem divider />
+                          <MenuItem eventKey='4'>Separated link</MenuItem>
+                        </DropdownButton>;
+    } else {
+      // if user is not logged in show login link
+      loginNavItem = <NavItemLink to={`/login`}>login</NavItemLink>;
+      signupNavItem = <NavItemLink to={`/signup`}>signup</NavItemLink>;
 
-			profileDropdown = null;
-		}
+      profileDropdown = null;
+    }
 
-		return (
-			<Navbar className="Navbar" brand={<Link to='/'>Isomorphic React</Link>}>
-				<Nav right>
-		      <NavItemLink to="/users">users</NavItemLink>
-		      <NavItemLink to="/chat">chat</NavItemLink>
-					{loginNavItem}
-					{signupNavItem}
-		      {profileDropdown}
-	    	</Nav>
-			</Navbar>
-			);
-	}
+    return (
+      <Navbar className="Navbar" brand={<Link to="/"><img height="25" src="/fitfam@2x.png" alt="FITFAM" /></Link>}>
+        <Nav right>
+          <NavItemLink to="/users">users</NavItemLink>
+          <NavItemLink to="/chat">chat</NavItemLink>
+          {loginNavItem}
+          {signupNavItem}
+          {profileDropdown}
+        </Nav>
+      </Navbar>
+      );
+  }
 }
 
 export default Header
