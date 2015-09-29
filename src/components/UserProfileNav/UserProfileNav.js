cbfb67ce64cdc3104d0565169fc7d066448fd893
adavkay <andykay.me@gmail.com>
2015-09-29 14:57:21 -0400

    padding fix

--- src/components/UserProfileNav/UserProfileNav.js
+++ src/components/UserProfileNav/UserProfileNav.js
@@ -16,24 +16,26 @@ class UserProfileNav extends React.Component {
 
   render() {
     return (
-      <div className="user-profile-nav">
-        <Nav bsStyle="pills">
-          <NavItemLink active={this._isActive('user-posts')} to="user-profile" params={{username: this.props.user.username}}>
-            Posts
-          </NavItemLink>
-          <NavItemLink active={this._isActive('user-workouts')} to="user-workouts" params={{username: this.props.user.username}}>
-            Workouts
-          </NavItemLink>
-          <NavItemLink active={this._isActive('user-photos')} to="user-photos" params={{username: this.props.user.username}}>
-            Photos
-          </NavItemLink>
-          <NavItemLink active={this._isActive('user-videos')} to="user-videos" params={{username: this.props.user.username}}>
-            Videos
-          </NavItemLink>
-          <NavItemLink active={this._isActive('user-songs')} to="user-songs" params={{username: this.props.user.username}}>
-            Songs
-          </NavItemLink>
-        </Nav>
+      <div className="user-profile-nav row">
+        <div className="col-xs-12">
+          <Nav bsStyle="pills">
+            <NavItemLink active={this._isActive('user-posts')} to="user-profile" params={{username: this.props.user.username}}>
+              Posts
+            </NavItemLink>
+            <NavItemLink active={this._isActive('user-workouts')} to="user-workouts" params={{username: this.props.user.username}}>
+              Workouts
+            </NavItemLink>
+            <NavItemLink active={this._isActive('user-photos')} to="user-photos" params={{username: this.props.user.username}}>
+              Photos
+            </NavItemLink>
+            <NavItemLink active={this._isActive('user-videos')} to="user-videos" params={{username: this.props.user.username}}>
+              Videos
+            </NavItemLink>
+            <NavItemLink active={this._isActive('user-songs')} to="user-songs" params={{username: this.props.user.username}}>
+              Songs
+            </NavItemLink>
+          </Nav>
+        </div>
       </div>
     );
   }
